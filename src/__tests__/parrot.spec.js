ad2734486c8a154d35631958ad8191e4d4d9e161
nicole <nikybg96@gmail.com>
2019-03-19 09:24:26 -0400

    refactor

--- src/__tests__/parrot.spec.js
+++ src/__tests__/parrot.spec.js
@@ -1,44 +1,44 @@
-import { Parrot, PARROT_TYPES } from '../parrot';
+import {PARROT_TYPES, African, Norwegian_Blue, European } from '../parrot';
 
 describe("Parrot", function () {
     test("get speed of european parrot", function () {
-        const parrot = new Parrot(PARROT_TYPES.EUROPEAN, 0, 0, false);
+        const parrot = new European(PARROT_TYPES.EUROPEAN);
 
         expect(parrot.getSpeed()).toBe(12);
     });
 
     test("get speed of african parrot with one coconut", function () {
-        const parrot = new Parrot(PARROT_TYPES.AFRICAN, 1, 0, false);
+        const parrot = new African(1);
 
         expect(parrot.getSpeed()).toBe(3);
     });
 
     test("get speed of african parrot with two coconuts", function () {
-        const parrot = new Parrot(PARROT_TYPES.AFRICAN, 2, 0, false);
+        const parrot = new African(2);
 
         expect(parrot.getSpeed()).toBe(0);
     });
 
     test("get speed of african parrot with no coconuts", function () {
-        const parrot = new Parrot(PARROT_TYPES.AFRICAN, 0, 0, false);
+        const parrot = new African(0);
 
         expect(parrot.getSpeed()).toBe(12);
     });
 
     test("get speed norwegian blue parrot nailed", function () {
-        const parrot = new Parrot(PARROT_TYPES.NORWEGIAN_BLUE, 0, 1.5, true);
+        const parrot = new Norwegian_Blue(1.5, true);
 
         expect(parrot.getSpeed()).toBe(0);
     });
 
     test("get speed norwegian blue parrot not nailed", function () {
-        const parrot = new Parrot(PARROT_TYPES.NORWEGIAN_BLUE, 0, 1.5, false);
+        const parrot = new Norwegian_Blue( 1.5, false);
 
         expect(parrot.getSpeed()).toBe(18);
     });
 
     test("get speed norwegian blue parrot not nailed high voltage", function () {
-        const parrot = new Parrot(PARROT_TYPES.NORWEGIAN_BLUE, 0, 4, false);
+        const parrot = new Norwegian_Blue(4, false);
 
         expect(parrot.getSpeed()).toBe(24);
     });
--- src/parrot.js
+++ src/parrot.js
@@ -5,34 +5,22 @@ export const PARROT_TYPES = {
 };
 
 export class Parrot {
-    constructor(type, numberOfCoconuts, voltage, isNailed) {
-        this.loadFactor = 9;
+    constructor(type, ) {
         this.baseSpeed = 12;
-
         this.type = type;
-        this.numberOfCoconuts = numberOfCoconuts;
-        this.voltage = voltage;
-        this.isNailed = isNailed;
     }
 
     getSpeed() {
-        switch (this.type) {
-
-            case PARROT_TYPES.EUROPEAN:
-                return new European(this.numberOfCoconuts, this.voltage, this.isNailed).getSpeed();
-            case PARROT_TYPES.AFRICAN:
-                return new African(this.numberOfCoconuts, this.voltage, this.isNailed).getSpeed();
-            case PARROT_TYPES.NORWEGIAN_BLUE:
-                return new Norwegian_Blue(this.numberOfCoconuts, this.voltage, this.isNailed).getSpeed();
-        }
         throw new Error("Should be unreachable");
     }
 
 }
 
-class African extends Parrot{
-    constructor(numberOfCoconuts, voltage, isNailed) {
-        super("AFRICAN",numberOfCoconuts, voltage, isNailed);
+export class African extends Parrot{
+    constructor(numberOfCoconuts) {
+        super("AFRICAN");
+        this.loadFactor = 9;
+        this.numberOfCoconuts = numberOfCoconuts;
     }
 
     getSpeed() {
@@ -40,22 +28,25 @@ class African extends Parrot{
     }
 }
 
-class Norwegian_Blue extends Parrot{
-    constructor(numberOfCoconuts, voltage, isNailed) {
-        super("NORWEGIAN",numberOfCoconuts, voltage, isNailed);
+export class Norwegian_Blue extends Parrot{
+    constructor(voltage, isNailed) {
+        super("NORWEGIAN");
+        this.voltage=voltage;
+        this.isNailed=isNailed;
     }
 
     getSpeed() {
         return (this.isNailed) ? 0 : this.getBaseSpeedWithVoltage(this.voltage);
     }
+
     getBaseSpeedWithVoltage(voltage) {
         return Math.min(24, voltage * this.baseSpeed);
     }
 }
 
-class European extends Parrot{
-    constructor(numberOfCoconuts, voltage, isNailed) {
-        super("EUROPEAN",numberOfCoconuts, voltage, isNailed);
+export class European extends Parrot{
+    constructor() {
+        super("EUROPEAN");
     }
 
     getSpeed() {
